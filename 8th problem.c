acc5efe2c2b7236e5e691ed66220f7906b79f3d1
padmadharshinikumar <padmadharshinikumar@gmail.com>
2026-01-29 07:35:34 +0530

    Add files via upload

---
+++ 8th problem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0;
+
+    scanf("%d", &n);
+
+    for (; n != 0; n = n / 10) {
+        rev = rev * 10 + (n % 10);
+    }
+
+    printf("%d", rev);
+
+    return 0;
+}
