6b60ceb5e5f065d2d766fc475889eb17c98b9718
padmadharshinikumar <padmadharshinikumar@gmail.com>
2026-01-29 07:36:02 +0530

    Add files via upload

---
+++ 10th problem.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a = 0, b = 1, c;
+
+    scanf("%d", &n);
+
+    if (n >= 1) {
+        printf("%d ", a);
+    }
+
+    if (n >= 2) {
+        printf("%d ", b);
+    }
+
+    for (i = 3; i <= n; i++) {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
