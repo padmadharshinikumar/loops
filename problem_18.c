4dd3030072030ace3944d514db07630747335f03
padmadharshinikumar <padmadharshinikumar@gmail.com>
2026-01-29 21:41:31 +0530

    Add files via upload

---
+++ problem_18.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        char ch = 'E' - i;
+
+        for (int j = 0; j <= i; j++) {
+            printf("%c ", ch);
+            ch++;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
