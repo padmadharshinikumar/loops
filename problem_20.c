a57e9a293fee38af6774886c9a3bbe4936c7c1a9
padmadharshinikumar <padmadharshinikumar@gmail.com>
2026-01-29 21:42:16 +0530

    Add files via upload

---
+++ problem_20.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main() {
+    int n = 5;
+
+    for (int i = 0; i < 2*n; i++)
+        printf("*");
+    printf("\n");
+
+ 
+    for (int i = n-1; i >= 1; i--) {
+        for (int j = 0; j < i; j++)
+            printf("*");
+        for (int j = 0; j < 2*(n-i); j++)
+            printf(" ");
+        for (int j = 0; j < i; j++)
+            printf("*");
+        printf("\n");
+    }
+
+   
+    for (int i = 1; i <= n-1; i++) {
+        for (int j = 0; j < i; j++)
+            printf("*");
+        for (int j = 0; j < 2*(n-i); j++)
+            printf(" ");
+        for (int j = 0; j < i; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    // Bottom full line
+    for (int i = 0; i < 2*n; i++)
+        printf("*");
+
+    return 0;
+}
