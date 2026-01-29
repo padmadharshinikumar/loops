f5e3872f86a3b740f1e9253ee512304693c19d29
padmadharshinikumar <padmadharshinikumar@gmail.com>
2026-01-29 07:34:23 +0530

    Add files via upload

---
+++ 6th problem.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int fact = 1;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        fact  = fact*i;
+    }
+
+    printf("%d", fact);
+
+    return 0;
+}
