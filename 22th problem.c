157a8e286d768b8b09d86c7462578d2a6db83aa8
padmadharshinikumar <padmadharshinikumar@gmail.com>
2026-01-29 07:39:46 +0530

    Add files via upload

---
+++ 22th problem.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, count = 0;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++) {
+        int isPrime = 1;
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime == 1) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
